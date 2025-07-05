a249beb4c5f1b9d773d76faa6c1cec6c234bb70f
iurie <nabiiru7@gmail.com>
2025-07-06 01:36:15 +0700

    make calculator with react + css

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,34 +1,71 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState("")
+
+  const handleClick = e => setResult(result.concat(e.target.id))
+
+  const handleClear = () => setResult("")
+
+  const deleteBtn = () => setResult(result.slice(0, -1))
+
+  const calculate = () => {
+    try {
+      const replaced = result.replace(/(\d+)%/g, (_, num) => `(${num}*0.01)`)
+      setResult(eval(replaced).toString())
+    } catch (error) {
+      setResult("Error")
+    }
+  }
+
+  const squareRoot = () => {
+    try {
+      const value = Math.sqrt(parseFloat(result))
+      setResult(value.toString())
+    } catch {
+      setResult("Error")
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="calculator">
+      <input type="text" value={result} disabled />
+
+      <div className="buttons">
+        
+        <button id="%" className="operator" onClick={handleClick}>%</button>
+        <button className="operator" onClick={squareRoot}>√</button>
+        <button className="operator" onClick={handleClear}>AC</button>
+       
+       <button className="operator" onClick={deleteBtn}>DEL</button>
+        <button id="(" className="operator" onClick={handleClick}>(</button>
+        <button id=")" className="operator" onClick={handleClick}>)</button>
+        <button id="." className="operator" onClick={handleClick}>.</button>
+        <button id="/" className="operator" onClick={handleClick}>:</button>
+
+   
+        <button id="7" className="number" onClick={handleClick}>7</button>
+        <button id="8" className="number" onClick={handleClick}>8</button>
+        <button id="9" className="number" onClick={handleClick}>9</button>
+        <button id="*" className="operator" onClick={handleClick}>x</button>
+
+        <button id="4" className="number" onClick={handleClick}>4</button>
+        <button id="5" className="number" onClick={handleClick}>5</button>
+        <button id="6" className="number" onClick={handleClick}>6</button>
+        <button id="-" className="operator" onClick={handleClick}>-</button>
+
+   
+        <button id="1" className="number" onClick={handleClick}>1</button>
+        <button id="2" className="number" onClick={handleClick}>2</button>
+        <button id="3" className="number" onClick={handleClick}>3</button>
+        <button id="+" className="operator" onClick={handleClick}>+</button>
+
+        <button id="00" className="number" onClick={handleClick}>00</button>
+        <button id="0" className="number" onClick={handleClick}>0</button>
+        <button id="=" className="counter" onClick={calculate}>=</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
